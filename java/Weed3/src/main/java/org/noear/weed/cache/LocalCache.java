8cf455d5db6c07ad7265cc588010699b956d4f77
noear <noear@live.cn>
2019-06-05 20:25:58 +0800

    3.1.6::重构本地缓存

--- java/Weed3/src/main/java/org/noear/weed/cache/LocalCache.java
+++ java/Weed3/src/main/java/org/noear/weed/cache/LocalCache.java
@@ -2,86 +2,65 @@ package org.noear.weed.cache;
 
 import org.noear.weed.ext.Fun1;
 
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
+import java.util.concurrent.*;
 
 /**
- * Created by yuety on 14-6-16.
- * 嵌入式缓存效率高；基于总量与超时双重管理(比自动超时要简单)
+ * Created by yuety on 19-06-05
+ * 嵌入式缓存效率高；
  *
  */
 public class LocalCache implements ICacheServiceEx {
     private String _cacheKeyHead;
     private int _defaultSeconds;
 
-    private int _max = 10000;
-    private int _count = 0;
-
-    private List<Integer> mks = new ArrayList<Integer>(); //key的顺序记录
-    private Map<Integer, LocalCacheRecord> mcc = new ConcurrentHashMap<Integer, LocalCacheRecord>();   //缓存存储器
+    private Map<String, LocalCacheRecord> _data = new ConcurrentHashMap<>();   //缓存存储器
+    private ScheduledExecutorService      _exec = Executors.newSingleThreadScheduledExecutor();
 
     public LocalCache(String keyHeader, int defSeconds) {
-        this(keyHeader, defSeconds, 10000);
-    }
-
-    public LocalCache(String keyHeader, int defSeconds, int recordMax) {
         _cacheKeyHead = keyHeader;
         _defaultSeconds = defSeconds;
-        _max = recordMax;
     }
 
     @Override
     public void store(String key, Object obj, int seconds) {
-        Integer hashKey = newKey(key);
-        LocalCacheRecord val = new LocalCacheRecord(obj, seconds);
+        remove(key);
 
-        mcc.put(hashKey, val);
-        mks.add(hashKey);
-
-        _count++;
-        if (_count > _max) //总量控制
-        {
-            Integer k = mks.get(0);
-            mcc.remove(k);
-            mks.remove(0);
-            _count--;
+        LocalCacheRecord val = new LocalCacheRecord(obj);
+        if(seconds>0){
+            val.future = _exec.schedule(()->{
+                _data.remove(key);
+            },seconds, TimeUnit.SECONDS);
         }
+
+        _data.put(key, val);
     }
 
     @Override
     public Object get(String key) {
-        Integer hashKey = newKey(key);
-        LocalCacheRecord val = mcc.get(hashKey);
+        LocalCacheRecord val = _data.get(key);
 
-        if (val == null)
-            return null;
-
-        if (val.timeout < System.currentTimeMillis()) {
-            mcc.remove(hashKey);
-            mks.remove(hashKey);
-            _count--;
-            return null;
-        } else
-            return val.data;
+        return val == null ? null : val.value;
     }
 
     @Override
     public void remove(String key) {
-        Integer hashKey = newKey(key);
-
-        mcc.remove(hashKey);
-        mks.remove(hashKey);
-        _count--;
+        LocalCacheRecord val = _data.remove(key);
+        if (val != null) {
+            if (val.future != null) {
+                val.future.cancel(true);
+            }
+        }
     }
 
     public void clear() {
-        mks.clear();
-        mcc.clear();
-        _count = 0;
-    }
+        for (LocalCacheRecord val : _data.values()) {
+            if (val.future != null) {
+                val.future.cancel(true);
+            }
+        }
 
-    private Integer newKey(String key){
-        return (_cacheKeyHead + "$" + key).hashCode();
+        _data.clear();
     }
 
     @Override
--- java/Weed3/src/main/java/org/noear/weed/cache/LocalCacheRecord.java
+++ java/Weed3/src/main/java/org/noear/weed/cache/LocalCacheRecord.java
@@ -1,16 +1,15 @@
 package org.noear.weed.cache;
 
-import java.util.Date;
+import java.util.concurrent.Future;
 
 /**
  * Created by yuety on 14/11/7.
  */
 class LocalCacheRecord {
-    public Object data;
-    public long timeout;
+    public Object value;
+    public Future future;
 
-    public LocalCacheRecord(Object val, int seconds) {
-        this.timeout = new Date().getTime() + seconds * 1000;
-        this.data = val;
+    public LocalCacheRecord(Object val) {
+        this.value = val;
     }
 }
