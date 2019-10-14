8a44e2f3f00a6a98399fcd09a3b5486b171e2563
noear <noear@live.cn>
2019-10-14 17:57:45 +0800

    3.2.0.1::添加xml mapper 支持

--- java/Weed3Demo/src/main/java/weed3demo/mapper/XmlSqlCompilerTest.java
+++ java/Weed3Demo/src/main/java/weed3demo/mapper/XmlSqlCompilerTest.java
@@ -3,18 +3,40 @@ package weed3demo.mapper;
 import org.junit.Test;
 import org.noear.weed.xml.*;
 
+import java.io.File;
+import java.net.URL;
+
 public class XmlSqlCompilerTest {
     @Test
     public  void load() throws Exception {
+        URL url = getResource("/weed3/mapper.xml");
 
-        XmlSqlLoader.load();
+        String code = XmlSqlCompiler.parse( new File(url.toURI()));
 
-        IXmlSqlBuilder tmp = XmlSqlFactory.get("org.xxx.xxx.user_add");
-        if(tmp == null){
-            return;
-        }else{
-            System.out.println("已找到 IXmlSqlBuilder: org.xxx.xxx.user_add");
+        System.out.println(code);
+
+//        XmlSqlLoader.load();
+//
+//        IXmlSqlBuilder tmp = XmlSqlFactory.get("org.xxx.xxx.user_add");
+//        if(tmp == null){
+//            return;
+//        }else{
+//            System.out.println("已找到 IXmlSqlBuilder: org.xxx.xxx.user_add");
+//        }
+    }
+
+    public static URL getResource(String name) {
+        URL url = XmlSqlLoader.class.getResource(name);
+        if (url == null) {
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            if (loader != null) {
+                url = loader.getResource(name);
+            } else {
+                url = ClassLoader.getSystemResource(name);
+            }
         }
+
+        return url;
     }
 
 
