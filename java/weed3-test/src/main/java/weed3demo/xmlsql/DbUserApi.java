f9cab1230a7fd15112945cd3eb9621a53d957530
noear <noear@live.cn>
2019-10-15 20:42:39 +0800

    3.2.0.4::框架，优化关键字处理；生成器：添加注释支持

--- java/weed3-test/src/main/java/weed3demo/xmlsql/DbUserApi.java
+++ java/weed3-test/src/main/java/weed3demo/xmlsql/DbUserApi.java
@@ -10,11 +10,16 @@ import org.noear.weed.xml.Namespace;
 
 @Namespace("weed3demo.xmlsql")
 public interface DbUserApi{
-  int user_add(int user_id);
-  int xxx(int user_id,String mobile);
-  int user_add_if(int user_id,String mobile);
-  void user_add_for(Collection<weed3demo.mapper.UserModel> list);
+  /** 添加用户 */
+  long user_add(int user_id);
+  /** 添加用户2 */
+  long user_add_if(int user_id,String mobile);
+  /** 批量添加用户3 */
+  long user_add_for(Collection<weed3demo.mapper.UserModel> list);
+  /** 删除一个用户 */
   void user_del(long user_id,int sex);
+  /** 更新一个用户，并清理相关相存 */
   void user_set(String mobile,int sex,String icon,long user_id);
+  /** 获取一批符合条件的用户 */
   List<weed3demo.mapper.UserModel> user_get(int foList,long user_id,String cols,String mobile);
 }
