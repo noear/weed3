8930f39685a27b46bd32f58b4a9cc3cb5158fb44
noear <noear@live.cn>
2019-10-27 09:16:06 +0800

    3.0.1.1

---
+++ java/weed3.cache.j2cache/src/main/java/org/noear/weed/cache/j2cache/J2Cache.java
@@ -0,0 +1,46 @@
+package org.noear.weed.cache.j2cache;
+
+import net.oschina.j2cache.CacheChannel;
+import org.noear.weed.cache.ICacheServiceEx;
+
+public class J2Cache implements ICacheServiceEx {
+    private String _cacheKeyHead;
+    private int _defaultSeconds;
+    private CacheChannel _cache;
+
+    public J2Cache(String keyHeader, int defSeconds) {
+        this(keyHeader,defSeconds,net.oschina.j2cache.J2Cache.getChannel());
+    }
+
+    public J2Cache(String keyHeader, int defSeconds,  CacheChannel cacheChannel) {
+        _cacheKeyHead = keyHeader;
+        _defaultSeconds = defSeconds;
+
+        _cache = cacheChannel;
+    }
+
+    @Override
+    public void store(String key, Object obj, int seconds) {
+        _cache.set(_cacheKeyHead, key, obj, seconds);
+    }
+
+    @Override
+    public Object get(String key) {
+        return _cache.get(_cacheKeyHead, key).getValue();
+    }
+
+    @Override
+    public void remove(String key) {
+        _cache.evict(_cacheKeyHead, key);
+    }
+
+    @Override
+    public int getDefalutSeconds() {
+        return _defaultSeconds;
+    }
+
+    @Override
+    public String getCacheKeyHead() {
+        return _cacheKeyHead;
+    }
+}
