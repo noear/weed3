d0b6ad920cbe570ade51761d2bfc057ab51f22b3
noear <noear@live.cn>
2019-10-27 08:50:07 +0800

    3.0.1.2

--- java/weed3.cache.redis/src/main/java/org/noear/weed/cache/redis/RedisCache.java
+++ java/weed3.cache.redis/src/main/java/org/noear/weed/cache/redis/RedisCache.java
@@ -1,7 +1,7 @@
 package org.noear.weed.cache.redis;
 
-import org.noear.snack.ONode;
 import org.noear.weed.cache.ICacheServiceEx;
+import org.noear.weed.cache.ISerializer;
 import org.noear.weed.utils.EncryptUtils;
 
 public class RedisCache implements ICacheServiceEx {
@@ -9,19 +9,26 @@ public class RedisCache implements ICacheServiceEx {
     private int _defaultSeconds;
 
     private RedisX _cache = null;
+    private ISerializer<String> _serializer = null;
 
-    public RedisCache(String keyHeader, int defSeconds, String server, String password, int db, int maxTotaol){
+    public RedisCache(String keyHeader, int defSeconds, String server, String password, int db, int maxTotaol) {
+        this(keyHeader, defSeconds, server, password, db, maxTotaol, Snack3Serializer.instance);
+    }
+
+    public RedisCache(String keyHeader, int defSeconds, String server, String password, int db, int maxTotaol, ISerializer<String> serializer) {
         _cacheKeyHead = keyHeader;
         _defaultSeconds = defSeconds;
 
-        _cache = new RedisX(server,password,db,maxTotaol);
+        _cache = new RedisX(server, password, db, maxTotaol);
+        _serializer = serializer;
     }
+
     @Override
     public void store(String key, Object obj, int seconds) {
         if (_cache != null) {
             String newKey = newKey(key);
             try {
-                String val = ONode.serialize(obj);
+                String val = _serializer.serialize(obj);
                 _cache.open0((ru) -> ru.key(newKey).expire(seconds).set(val));
 
             } catch (Exception ex) {
@@ -35,14 +42,14 @@ public class RedisCache implements ICacheServiceEx {
     public Object get(String key) {
         if (_cache != null) {
             String newKey = newKey(key);
-            String val = _cache.open1((ru)->  ru.key(newKey).get());
+            String val = _cache.open1((ru) -> ru.key(newKey).get());
             try {
-                return ONode.deserialize(val, Object.class);
-            }catch (Exception ex){
+                return _serializer.deserialize(val);
+            } catch (Exception ex) {
                 ex.printStackTrace();
                 throw new RuntimeException(ex);
             }
-        }else{
+        } else {
             return null;
         }
     }
@@ -51,7 +58,7 @@ public class RedisCache implements ICacheServiceEx {
     public void remove(String key) {
         if (_cache != null) {
             String newKey = newKey(key);
-            _cache.open0((ru)->{
+            _cache.open0((ru) -> {
                 ru.key(newKey).delete();
             });
         }
