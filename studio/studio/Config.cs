0778deff01d195dec9fd69f1646a486d2b940d16
noear <noear@live.cn>
2019-10-27 09:20:34 +0800

    3.2.1.6

--- studio/studio/Config.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-namespace weedstudio {
-    public static class Config {
-        public static readonly string DIR_OUT = "out";
-        public static readonly string DIR_BUILDER = "res/builder";
-        public static readonly string DIR_TEMPLET = "res/templet";
-    }
-}
