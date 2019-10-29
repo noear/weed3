1ed0a329628639675c4d519c14ee61d40cb79d8f
noear <noear@live.cn>
2019-10-29 10:30:48 +0800

    3.2.2

--- _demo/_demo.weed3_springboot/src/main/java/webapp/App.java
+++ _demo/_demo.weed3_springboot/src/main/java/webapp/App.java
@@ -1,5 +1,8 @@
 package webapp;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class App {
     public static void main(String[] args){
