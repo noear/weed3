1f53c5e884b4327e4820864a6db721896ba3907a
noear <noear@live.cn>
2019-11-15 18:28:18 +0800

    3.2.2.1

--- weed3-maven-plugin/src/main/java/org/noear/weed/generator/xml/TypeBlock.java
+++ weed3-maven-plugin/src/main/java/org/noear/weed/generator/xml/TypeBlock.java
@@ -16,8 +16,9 @@ public class TypeBlock {
             return;
         }
 
-        oldType = type.replace("[","<").replace("]",">");
+        oldType = type.replace("[", "<").replace("]", ">");
         slmType = oldType;
+
 
         int idx0 = oldType.lastIndexOf(".");
         if (idx0 > 0) {
@@ -28,11 +29,19 @@ public class TypeBlock {
             if (idx2 > 0) {
                 slmType = slmType.substring(0, idx2);
             }
+        } else {
+            if (oldType.indexOf(">") > 0) { //List<Long> ，没有.，但有 <
+                int idx1 = oldType.indexOf("<");
+                slmType = oldType.substring(idx1 + 1, oldType.length() - 1);
+            }
         }
 
         if (oldType.indexOf(">") > 0) {
             int idx1 = oldType.indexOf("<");
             impType = oldType.substring(idx1 + 1, oldType.length() - 1);
+            if (impType.indexOf(".") < 0) {
+                impType = null; //如果不是fullName，不导入了
+            }
 
             newType = oldType.substring(0, idx1) + "<" + slmType + ">";
         } else {
