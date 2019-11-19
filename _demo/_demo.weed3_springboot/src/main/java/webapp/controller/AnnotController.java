e96b753bacd4d0a328de91a8dd1dab1d575b940d
noear <noear@live.cn>
2019-11-19 21:34:38 +0800

    3.2.2.4

---
+++ _demo/_demo.weed3_springboot/src/main/java/webapp/controller/AnnotController.java
@@ -0,0 +1,54 @@
+package webapp.controller;
+
+import org.noear.weed.DbContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import webapp.dso.SqlAnnotation;
+
+
+@RequestMapping("/annot")
+@RestController
+public class AnnotController {
+    @Autowired
+    @Qualifier("db2")
+    DbContext db2;
+
+    @RequestMapping("demo0/html")
+    public ModelAndView demo0() throws Exception {
+        ModelAndView mv = new ModelAndView("view.ftl");
+
+        Object _map = demo3();
+        mv.addObject("map", _map);
+
+        return mv;
+    }
+
+    @RequestMapping("demo1/json")
+    public Object demo1() throws Exception {
+        return db2.mapper(SqlAnnotation.class).appx_get();
+    }
+
+    @RequestMapping("demo2/json")
+    public Object demo2() throws Exception {
+        return db2.mapper(SqlAnnotation.class).appx_get2(48);
+    }
+
+    @RequestMapping("demo3/json")
+    public Object demo3() throws Exception {
+        return db2.mapper(SqlAnnotation.class).appx_get3("appx",48);
+    }
+
+    @RequestMapping("demo4/json")
+    public Object demo4() throws Exception {
+        return db2.mapper(SqlAnnotation.class).appx_getlist(1);
+    }
+
+    @RequestMapping("demo5/json")
+    public Object demo5() throws Exception {
+        return db2.mapper(SqlAnnotation.class).appx_getids();
+    }
+
+}
---
+++ _demo/_demo.weed3_springboot/src/main/java/webapp/controller/JavaController.java
@@ -0,0 +1,106 @@
+package webapp.controller;
+
+import org.noear.weed.DbContext;
+import org.noear.weed.cache.ICacheServiceEx;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import webapp.model.AppxModel;
+
+
+
+@RequestMapping("/java")
+@RestController
+public class JavaController {
+    @Autowired
+    @Qualifier("db2")
+    DbContext db2;
+
+    @Autowired
+    @Qualifier("cache")
+    ICacheServiceEx cache;
+
+    @RequestMapping("demo0/html")
+    public ModelAndView demo0() throws Exception {
+        ModelAndView mv = new ModelAndView("view.ftl");
+
+        Object _map = demo3();
+        mv.addObject("map", _map);
+
+        return mv;
+    }
+
+    @RequestMapping("demo1/json")
+    public Object demo1() throws Exception {
+        //
+        // select app_id from appx limit 1
+        //
+        return db2.table("appx")
+                .limit(1)
+                .select("app_id")
+                .getValue();
+    }
+
+    @RequestMapping("demo2/json")
+    public Object demo2() throws Exception {
+        //
+        // select * from appx where app_id = @{app_id} limit 1
+        //
+        int app_id = 48;
+
+        return db2.table("appx")
+                .whereEq("app_id", app_id)
+                .limit(1)
+                .select("*")
+                .caching(cache)
+                .cacheTag("app_" + app_id)
+                .getItem(AppxModel.class);
+    }
+
+    @RequestMapping("demo3/json")
+    public Object demo3() throws Exception {
+        //
+        // select * from ${tb} where app_id = @{app_id} limit 1
+        //
+        int app_id = 48;
+        String tb = "appx";
+
+        Object tmp = db2.table(tb)
+                .whereEq("app_id", app_id)
+                .limit(1)
+                .select("*")
+                .getMap();
+
+        cache.clear("test");
+
+        return tmp;
+    }
+
+    @RequestMapping("demo4/json")
+    public Object demo4() throws Exception {
+        int app_id = 48;
+
+        //
+        // select * from appx where app_id>@{app_id} order by app_id asc limit 4
+        //
+        return db2.table("appx")
+                .where("app_id>?",48)
+                .orderBy("app_id ASC")
+                .limit(4)
+                .select("*")
+                .getList(AppxModel.class);
+    }
+
+    @RequestMapping("demo5/json")
+    public Object demo5() throws Exception {
+        //
+        // select app_id from appx limit 4
+        //
+        return db2.table("appx")
+                .limit(4)
+                .select("app_id")
+                .getArray(0);
+    }
+}
