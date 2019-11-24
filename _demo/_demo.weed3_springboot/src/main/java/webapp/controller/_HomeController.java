56c6a84446bff25804ddab47f3fdc447285584dd
noear <noear@live.cn>
2019-11-24 12:31:13 +0800

    3.2.3

--- _demo/_demo.weed3_springboot/src/main/java/webapp/controller/_HomeController.java
+++ _demo/_demo.weed3_springboot/src/main/java/webapp/controller/_HomeController.java
@@ -1,19 +1,15 @@
 package webapp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletResponse;
 
 @RequestMapping("/")
-@RestController
+@Controller
 public class _HomeController {
-    //
-    // 好久没弄 spring boot 了，视图没弄出来
-    //
-    @RequestMapping("")
-    public void home(HttpServletResponse response) throws Exception{
-        response.sendRedirect("/nav.htm");
+
+    @RequestMapping("/")
+    public String home() {
+        return "nav";
     }
 }
