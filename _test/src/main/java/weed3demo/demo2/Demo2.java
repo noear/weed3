5065892b9dd03a7f7af6586110de7f78bb1be676
noear <noear@live.cn>
2019-12-12 12:37:32 +0800

    Update Demo2.java

--- _test/src/main/java/weed3demo/demo2/Demo2.java
+++ _test/src/main/java/weed3demo/demo2/Demo2.java
@@ -7,31 +7,43 @@ import weed3demo.DbUtil;
 public class Demo2 {
     DbContext db = DbUtil.db;
 
-    public Object demo1(String name) throws Exception {
-        DbTableQuery qr = db.table("appx");
+    public Object demo1(String name, String akey) throws Exception {
+        DbTableQuery qr = db.table("appx").where("1=1");
+
         if (name != null) {
-            qr.where("name=?", name);
+            qr.and("name=?", name);
         }
+
+        if (akey != null) {
+            qr.and("akey=?", akey);
+        }
+
         return qr.limit(1).select("*").getMapList();
     }
 
-    public Object demo1_2(String name) throws Exception {
-        return db.table("appx").build((qr) -> {
+    public Object demo1_2(String name, String akey) throws Exception {
+        return db.table("appx").where("1=1").build((qr) -> {
             if (name != null) {
-                qr.where("name=?", name);
+                qr.and("name=?", name);
+            }
+
+            if (akey != null) {
+                qr.and("akey=?", akey);
             }
         }).limit(1).select("*").getMapList();
     }
 
-    public Object demo1_3(String name) throws Exception {
+    public Object demo1_3(String name, String akey) throws Exception {
         return db.table("appx")
-                .whereIf(name != null, "name=?", name)
+                .where("1=1")
+                .andIf(name != null, "name=?", name)
+                .andIf(akey != null, "akey=?", akey)
                 .limit(1).select("*").getMapList();
     }
 
 
     public void demo2(String name, String note, String akey) throws Exception {
-        DbTableQuery qr = db.table("appx").set("log_fulltime","$NOW()");
+        DbTableQuery qr = db.table("appx").set("log_fulltime", "$NOW()");
 
         if (name != null) {
             qr.set("name", name);
@@ -49,7 +61,7 @@ public class Demo2 {
     }
 
     public void demo2_2(String name, String note, String akey) throws Exception {
-        db.table("appx").set("log_fulltime","$NOW()").build(qr->{
+        db.table("appx").set("log_fulltime", "$NOW()").build(qr -> {
             if (name != null) {
                 qr.set("name", name);
             }
@@ -66,10 +78,10 @@ public class Demo2 {
 
     public void demo2_3(String name, String note, String akey) throws Exception {
         db.table("appx")
-          .set("log_fulltime","$NOW()")
-          .setIf(name != null, "name", name)
-          .setIf(note != null, "note", note)
-          .setIf(akey != null, "akey", akey)
-          .insert();
+                .set("log_fulltime", "$NOW()")
+                .setIf(name != null, "name", name)
+                .setIf(note != null, "note", note)
+                .setIf(akey != null, "akey", akey)
+                .insert();
     }
 }
