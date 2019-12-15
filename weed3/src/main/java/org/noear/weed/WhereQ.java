3186c36d985866cb44b47362cfebb28ca5c18403
noear <noear@live.cn>
2019-12-15 19:20:04 +0800

    3.2.3.10

--- weed3/src/main/java/org/noear/weed/WhereQ.java
+++ weed3/src/main/java/org/noear/weed/WhereQ.java
@@ -1,11 +1,11 @@
 package org.noear.weed;
 
 public class WhereQ extends WhereBase<WhereQ> {
-    public WhereQ(DbContext context) {
+    protected WhereQ(DbContext context) {
         super(context);
     }
 
-    public WhereQ(DbTableQuery query){
+    protected WhereQ(DbTableQuery query){
         super();
         _context = query._context;
         _builder = query._builder;
