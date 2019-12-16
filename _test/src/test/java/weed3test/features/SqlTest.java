8da0e945af2a7ec1bb3a65a8d80db9e65d44c5bc
noear <noear@live.cn>
2019-12-16 22:46:32 +0800

    3.2.3.14

--- _test/src/test/java/weed3test/features/SqlTest.java
+++ _test/src/test/java/weed3test/features/SqlTest.java
@@ -5,6 +5,9 @@ import org.noear.weed.DbContext;
 import weed3test.DbUtil;
 import weed3test.model.AppxModel;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SqlTest {
     DbContext db = DbUtil.db;
 
@@ -13,5 +16,11 @@ public class SqlTest {
         assert db.sql("select * from appx where app_id=?", 32)
                 .getItem(AppxModel.class)
                 .app_id == 32;
+    }
+
+    public void test2() throws Exception {
+        Map<String, Object> map = new HashMap<>();
+
+        db.table("appx").setMap(map).upsert("akey");
     }
 }
