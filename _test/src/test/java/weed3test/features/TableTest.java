9974ac2277e800b83fa1dd35bb7e9e595c0a9952
noear <noear@live.cn>
2019-12-17 10:05:53 +0800

    3.2.3.16

--- _test/src/test/java/weed3test/features/TableTest.java
+++ _test/src/test/java/weed3test/features/TableTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.noear.weed.BaseMapper;
 import org.noear.weed.DbContext;
 import weed3test.DbUtil;
+import weed3test.model.AgroupModel;
 import weed3test.model.AppxModel;
 
 public class TableTest {
@@ -21,10 +22,32 @@ public class TableTest {
 
     @Test
     public void test2() throws Exception {
+        assert db.table("appx a")
+                 .innerJoin("appx_agroup g").onEq("a.agroup_id", "g.agroup_id")
+                 .whereEq("app_id", 22)
+                 .select("*,g.name gname")
+                 .getItem(AppxModel.class).app_id == 22;
+
+        System.out.println(db.lastCommand.text);
+    }
+
+    @Test
+    public void test11() throws Exception {
         assert db.table(AppxModel.class)
                 .whereEq(AppxModel::getApp_id, 21)
                 .select("*")
                 .getItem(AppxModel.class).app_id == 21;
+
+        System.out.println(db.lastCommand.text);
+    }
+
+    @Test
+    public void test12() throws Exception {
+        assert db.table(AppxModel.class)
+                 .innerJoin(AgroupModel.class).onEq(AppxModel::getAgroup_id,AgroupModel::getAgroup_id)
+                 .whereEq(AppxModel::getApp_id, 22)
+                 .select("*,t1.name gname")
+                 .getItem(AppxModel.class).app_id == 22;
 
         System.out.println(db.lastCommand.text);
     }
---
+++ _test/src/test/java/weed3test/model/AgroupModel.java
@@ -0,0 +1,14 @@
+package weed3test.model;
+
+import lombok.Data;
+import org.noear.weed.annotation.PrimaryKey;
+import org.noear.weed.annotation.Table;
+
+//和表名不相同，须注解表名
+@Data
+@Table("appx_agroup")
+public class AgroupModel {
+    @PrimaryKey
+    public Integer agroup_id;
+    public String name;
+}
