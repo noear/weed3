f827681e04669ec152fba74f6e6759bb0368405a
noear <noear@live.cn>
2019-12-18 09:22:48 +0800

    3.2.4.1

--- _test/src/test/java/weed3test/features/TranDemo.java
+++ _test/src/test/java/weed3test/features/TranDemo.java
@@ -9,14 +9,14 @@ public class TranDemo {
         DbContext db1 = DbUtil.db;
         DbContext db2 = DbUtil.db;
 
-        new DbTranQueue().execute((tq) -> {
-            db1.tran().join(tq).execute(t -> {
-                db1.sql("").insert();
-            });
+new DbTranQueue().execute((tq) -> {
+    db1.tran().join(tq).execute(t -> {
+        db1.sql("").insert();
+    });
 
-            db2.tran().join(tq).execute(t -> {
-                db2.sql("").update();
-            });
-        });
+    db2.tran().join(tq).execute(t -> {
+        db2.sql("").update();
+    });
+});
     }
 }
