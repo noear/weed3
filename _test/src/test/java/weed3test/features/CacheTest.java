1646b2c0f323a3b822ba8b35017d48042931acc7
noear <noear@live.cn>
2019-12-19 15:03:57 +0800

    3.2.4.2

---
+++ _test/src/test/java/weed3test/features/CacheTest.java
@@ -0,0 +1,30 @@
+package weed3test.features;
+
+import org.junit.Test;
+import org.noear.weed.DataItem;
+import org.noear.weed.DbContext;
+import org.noear.weed.cache.ICacheServiceEx;
+import org.noear.weed.cache.LocalCache;
+import weed3test.DbUtil;
+import weed3test.model.AppxModel;
+
+public class CacheTest {
+    DbContext db2 = DbUtil.db;
+    ICacheServiceEx cache = new LocalCache();
+
+    @Test
+    public void test1() throws Exception {
+        AppxModel tmp = db2.table("appx")
+                .where("app_id=?", 23)
+                .caching(cache)
+                .select("*")
+                .cacheTag("app_23")
+                .getItem(AppxModel.class);
+
+        cache.tags().update("app_23", (DataItem di) -> {
+            AppxModel m = di.toEntity(AppxModel.class);
+            assert m.app_id == 23;
+            return di;
+        });
+    }
+}
