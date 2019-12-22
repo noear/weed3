237310e50f6f04d1786e41c6f5728173a1420b8f
noear <noear@live.cn>
2019-12-22 23:02:00 +0800

    3.2.5.2

--- _test/src/test/java/weed3test/DbUtil.java
+++ _test/src/test/java/weed3test/DbUtil.java
@@ -92,7 +92,7 @@ public class DbUtil {
             System.out.println(":::"+cmd.text);
         });
 
-        Map<String, String> map =  dbMysqlCfg(); // dbOracleCfg(); //dbPgsqlCfg(); //dbMssqlCfg();//
+        Map<String, String> map =  dbMysqlCfg(); // dbOracleCfg(); // dbPgsqlCfg(); //dbMssqlCfg();//
 
         DbContext db = new DbContext(map.get("schema"), dataSource(map)).nameSet("rock");
         WeedConfig.isUsingSchemaPrefix =true;
