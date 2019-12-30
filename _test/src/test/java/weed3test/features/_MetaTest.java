09987f6baf06ba2d2392d18e88c6ee416b20fcbb
noear <noear@live.cn>
2019-12-30 13:16:41 +0800

    3.2.6.4

--- _test/src/test/java/weed3test/features/_MetaTest.java
+++ _test/src/test/java/weed3test/features/_MetaTest.java
@@ -2,28 +2,29 @@ package weed3test.features;
 
 import org.junit.Test;
 import org.noear.weed.DbContext;
-import org.noear.weed.wrap.ColumnWrap;
 import org.noear.weed.wrap.TableWrap;
+import weed3builder.EntityBuilder;
 import weed3test.DbUtil;
-import weed3test.dso.JavaDbType;
-
-import java.sql.Timestamp;
-import java.util.Date;
 
 public class _MetaTest {
     DbContext db = DbUtil.db;
 
     @Test
     public void test1() {
-
         for (TableWrap tw : db.dbTables()) {
-            String code = EntityBuilder.buildByTable(tw);
+            String code = EntityBuilder.buildByTable(null, tw);
             System.out.println(code);
             //break;
         }
     }
 
+    @Test
     public void test2(){
+        System.out.println(System.getProperty("user.dir"));
+    }
 
+    @Test
+    public void test3() throws Exception{
+//       EntityBuilder.createByDb("demo",db);
     }
 }
