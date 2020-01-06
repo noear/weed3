fd491f9ab8710a3d2a198a6d23eec53c7fc7ef63
noear <noear@live.cn>
2020-01-06 18:12:19 +0800

    Create SqlMapper.java

---
+++ _test/src/main/java/webapp/dso/SqlMapper.java
@@ -0,0 +1,30 @@
+package webapp.dso;
+
+import java.math.*;
+import java.time.*;
+import java.util.*;
+
+import org.noear.weed.BaseMapper;
+import org.noear.weed.DataItem;
+import org.noear.weed.DataList;
+import org.noear.weed.annotation.Db;
+import org.noear.weed.xml.Namespace;
+
+import webapp.model.AppxModel;
+
+@Db("db")
+@Namespace("webapp.dso")
+public interface SqlMapper{
+    //随便取条数据的ID
+    int appx_get() throws Exception;
+
+    //根据id取条数据
+    AppxModel appx_get2(int app_id) throws Exception;
+
+    //取一批ID
+    Map<String,Object> appx_get3(String tb, int app_id) throws Exception;
+
+    List<AppxModel> appx_getlist(int app_id) throws Exception;
+
+    List<Integer> appx_getids() throws Exception;
+}
