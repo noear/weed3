de5d81cb402f6c61ef9669c0641a8f58df9d37ff
noear <noear@live.cn>
2020-02-09 18:01:50 +0800

    3.2.7.1

---
+++ _test/src/test/java/weed3test/model/AppxModel2.java
@@ -0,0 +1,47 @@
+package weed3test.model;
+
+import lombok.Data;
+import org.noear.weed.GetHandlerEx;
+import org.noear.weed.IBinder;
+
+import java.util.Date;
+
+//和表名不相同，须注解表名
+@Data
+public class AppxModel2 implements IBinder{
+    public int app_id;
+    public String app_key;
+    public String akey;
+
+    public int agroup_id;
+    public int ugroup_id;
+
+    public String name;
+    public String note;
+
+    private int _ar_is_examine;
+    private int _ar_examine_ver;
+
+    public void bind(GetHandlerEx s)
+    {
+        //1.source:数据源
+        //
+        app_id = s.get("app_id").value(0);
+        app_key = s.get("app_key").value(null);
+        akey = s.get("akey").value(null);
+
+        agroup_id = s.get("agroup_id").value(0);
+        ugroup_id = s.get("ugroup_id").value(0);
+
+        name = s.get("name").value(null);
+        note = s.get("note").value(null);
+
+        _ar_is_examine = s.get("ar_is_examine").value(0);
+        _ar_examine_ver = s.get("ar_examine_ver").value(0);
+    }
+
+    public IBinder clone()
+    {
+        return new AppxModel2();
+    }
+}
