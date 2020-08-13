c16974dc7c9a20660f4c0048997fcb9eebf73f92
noear <noear@live.cn>
2020-08-13 16:57:40 +0800

    3.2.9

--- _test/src/test/java/weed3test/features/TranDemo.java
+++ _test/src/test/java/weed3test/features/TranDemo.java
@@ -19,28 +19,28 @@ public class TranDemo {
         DbContext db2 = DbUtil.db;
 
         new DbTranQueue().execute((tq) -> {
-            db1.tran().join(tq).execute(t -> {
+            db1.tran(tq, t -> {
                 db1.sql("").insert();
             });
 
-            db2.tran().join(tq).execute(t -> {
+            db2.tran(tq, t -> {
                 db2.sql("").update();
             });
         });
     }
 
-    public void test2() throws Exception{
+    public void test2() throws Exception {
         DbContext db1 = DbUtil.db;
         DbContext db2 = DbUtil.db;
 
         new DbTranQueue().execute((tq) -> {
             VarHolder<Long> tmp = new VarHolder<>();
 
-            db1.tran().join(tq).execute(t -> {
+            db1.tran(tq, t -> {
                 tmp.value = db1.sql("").insert();
             });
 
-            db2.tran().join(tq).execute(t -> {
+            db2.tran(tq, t -> {
                 db2.sql("").update();
             });
         });
