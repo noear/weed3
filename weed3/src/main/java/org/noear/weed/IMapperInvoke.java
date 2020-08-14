c9aadd86d7e5a45bc3ba2579c68cc1058715b5c3
noear <noear@live.cn>
2020-08-15 01:49:11 +0800

    3.2.10

--- weed3/src/main/java/org/noear/weed/IMapperInvoke.java
+++ weed3/src/main/java/org/noear/weed/IMapperInvoke.java
@@ -2,6 +2,8 @@ package org.noear.weed;
 
 import org.noear.weed.wrap.MethodWrap;
 
+import java.sql.SQLException;
+
 public interface IMapperInvoke {
     Object call(Object proxy, DbContext db, String sqlid, Class<?> caller, MethodWrap mWrap, Object[] args) throws Throwable;
 }
