fb43e84e9c2bddde389dc4eedc336bf8ad0942a1
noear <noear@live.cn>
2020-08-21 22:16:00 +0800

    3.2.13

---
+++ weed3/src/main/java/org/noear/weed/TmlCache.java
@@ -0,0 +1,10 @@
+package org.noear.weed;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TmlCache {
+    public String sql;
+    public String sql2;
+    public List<TmlMark> marks = new ArrayList<>();
+}
