454e08b61676a0e8742b3d6b1be6e80b6c616584
noear <noear@live.cn>
2020-08-24 15:46:28 +0800

    3.2.13

--- _test/src/test/java/weed3test/DbUtil.java
+++ _test/src/test/java/weed3test/DbUtil.java
@@ -147,7 +147,7 @@ public class DbUtil {
             System.out.println(":::"+cmd.text);
         });
 
-        HikariDataSource source = dbSqliteCfg(); //dbMysqlCfg(); // dbOracleCfg(); //  dbPgsqlCfg(); // dbMssqlCfg(); //
+        HikariDataSource source = dbMysqlCfg(); //dbSqliteCfg(); // dbOracleCfg(); //  dbPgsqlCfg(); // dbMssqlCfg(); //
 
         DbContext db = new DbContext(source.getSchema(), source).nameSet("rock");
         //WeedConfig.isUsingSchemaPrefix =true;
