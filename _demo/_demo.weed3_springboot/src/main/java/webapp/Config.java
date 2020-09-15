f6b8799d1471f50d8d65db3e05e60d1cf1814418
noear <noear@live.cn>
2020-09-15 16:22:14 +0800

    3.2.17

--- _demo/_demo.weed3_springboot/src/main/java/webapp/Config.java
+++ _demo/_demo.weed3_springboot/src/main/java/webapp/Config.java
@@ -5,6 +5,7 @@ import org.noear.weed.DbContext;
 import org.noear.weed.cache.ICacheServiceEx;
 import org.noear.weed.cache.LocalCache;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
