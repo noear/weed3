47947c2559df3bdffe478c05f6ed8e0d9448eae1
noear <noear@live.cn>
2020-09-15 17:25:59 +0800

    3.2.17

--- _demo/_demo.weed3_springboot/src/main/java/webapp/controller/_HomeController.java
+++ _demo/_demo.weed3_springboot/src/main/java/webapp/controller/_HomeController.java
@@ -2,10 +2,11 @@ package webapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 
-@Controller
+@RestController
 public class _HomeController {
 
     @RequestMapping("/")
