1bf81cdc92babf155b39a00bf7ae273a5677b57b
noear <noear@live.cn>
2020-09-15 17:00:30 +0800

    3.2.17

--- _demo/_demo.weed3_springboot/src/main/java/webapp/Config.java
+++ _demo/_demo.weed3_springboot/src/main/java/webapp/Config.java
@@ -5,6 +5,8 @@ import org.noear.weed.DbContext;
 import org.noear.weed.cache.ICacheServiceEx;
 import org.noear.weed.cache.LocalCache;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -26,15 +28,22 @@ public class Config {
     }
 
 
-    @Bean(name = "dataSource")
+
+    @Bean(name = "db1p")
     @ConfigurationProperties(prefix = "test.db1")
-    public DataSource dataSource() {
-        return new HikariDataSource();
+    public DataSourceProperties dataSource() {
+        return new DataSourceProperties();
     }
 
     @Bean("db1")
     @Primary
-    public DbContext db2(@Qualifier("dataSource") DataSource dataSource){
+    public DbContext db2(@Qualifier("db1p") DataSourceProperties db1p){
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(db1p.getUrl());
+        dataSource.setUsername(db1p.getUsername());
+        dataSource.setPassword(db1p.getPassword());
+        dataSource.setDriverClassName(db1p.getDriverClassName());
+
         DataSourceHelper.initData(dataSource);
 
         return new DbContext(dataSource);
