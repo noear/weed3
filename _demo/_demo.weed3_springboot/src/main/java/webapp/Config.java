929d5da814eb18f6f8e2fd2fc599f5075f75a121
noear <noear@live.cn>
2020-09-15 17:17:39 +0800

    3.2.17

--- _demo/_demo.weed3_springboot/src/main/java/webapp/Config.java
+++ _demo/_demo.weed3_springboot/src/main/java/webapp/Config.java
@@ -1,23 +1,32 @@
 package webapp;
 
 import com.zaxxer.hikari.HikariDataSource;
+import lombok.Setter;
 import org.noear.weed.DbContext;
 import org.noear.weed.cache.ICacheServiceEx;
 import org.noear.weed.cache.LocalCache;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import webapp.dso.DataSourceHelper;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
-@Configuration
+@Setter
+@ConfigurationProperties("test.db1")
 public class Config {
+
+    @Autowired
+    ApplicationContext ctx;
+
     //
     //cache
     //
@@ -27,22 +36,18 @@ public class Config {
         return new LocalCache("test",60).nameSet("test");
     }
 
-
-
-    @Bean(name = "db1p")
-    @ConfigurationProperties(prefix = "test.db1")
-    public DataSourceProperties dataSource() {
-        return new DataSourceProperties();
-    }
+    private String url;
+    private String driverClassName;
+    private Properties db1;
 
     @Bean("db1")
     @Primary
-    public DbContext db2(@Qualifier("db1p") DataSourceProperties db1p){
+    public DbContext db2(){
         HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(db1p.getUrl());
-        dataSource.setUsername(db1p.getUsername());
-        dataSource.setPassword(db1p.getPassword());
-        dataSource.setDriverClassName(db1p.getDriverClassName());
+        dataSource.setJdbcUrl(db1.getProperty("url"));
+        dataSource.setUsername(db1.getProperty("username"));
+        dataSource.setPassword(db1.getProperty("password"));
+        dataSource.setDriverClassName(db1.getProperty("driverClassName"));
 
         DataSourceHelper.initData(dataSource);
 
