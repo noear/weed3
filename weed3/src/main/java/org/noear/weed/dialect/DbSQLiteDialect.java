f095a41474f28057f19984fb7e4a8b6dba864ebd
noear <noear@live.cn>
2020-09-19 21:17:48 +0800

    3.2.22

--- weed3/src/main/java/org/noear/weed/dialect/DbSQLiteDialect.java
+++ weed3/src/main/java/org/noear/weed/dialect/DbSQLiteDialect.java
@@ -3,14 +3,45 @@ package org.noear.weed.dialect;
 import org.noear.weed.DbContext;
 import org.noear.weed.SQLBuilder;
 
+import java.sql.Clob;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DbSQLiteDialect implements DbDialect {
     @Override
+    public Object preChange(Object val) throws SQLException {
+        if (val instanceof Clob) {
+            Clob clob = ((Clob) val);
+            return clob.getSubString(1, (int) clob.length());
+        } else if (val instanceof Byte) {
+            return ((Byte) val).byteValue() > 0;
+        } else {
+            return val;
+        }
+    }
+
+    @Override
     public ResultSet getTables(DatabaseMetaData md, String catalog, String schema) throws SQLException {
         return md.getTables(null, null, null, new String[]{"TABLE"});
+    }
+
+    @Override
+    public String preReview(String code) {
+        if (code.indexOf("CREATE TABLE") >= 0) {
+            return code.replace("ENGINE=InnoDB ", "")
+                    .replace("USING BTREE", "")
+                    .replace("USING HASH", "")
+                    .replaceAll("`\\(\\d+\\)\\)", "`)")
+                    .replace("CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ", "");
+
+        }
+
+        if(code.indexOf("information_schema.")>=0){
+            return  code.toUpperCase();
+        }
+
+        return code;
     }
 
     @Override
