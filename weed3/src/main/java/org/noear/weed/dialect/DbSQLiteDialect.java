21f3dd0e9b4834d51c5b2ded9eeda8664a2f97b9
noear <noear@live.cn>
2020-09-19 17:30:17 +0800

    3.2.22

--- weed3/src/main/java/org/noear/weed/dialect/DbSQLiteDialect.java
+++ weed3/src/main/java/org/noear/weed/dialect/DbSQLiteDialect.java
@@ -3,7 +3,16 @@ package org.noear.weed.dialect;
 import org.noear.weed.DbContext;
 import org.noear.weed.SQLBuilder;
 
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DbSQLiteDialect implements DbDialect {
+    @Override
+    public ResultSet getTables(DatabaseMetaData md, String catalog, String schema) throws SQLException {
+        return md.getTables(null, null, null, new String[]{"TABLE"});
+    }
+
     @Override
     public void selectPage(DbContext ctx, String table1, SQLBuilder sqlB, StringBuilder orderBy, int start, int size) {
         sqlB.insert(0,"SELECT ");
