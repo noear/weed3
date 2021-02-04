58e9a4ceda44f1f63aa1d33671466dd412eb67c5
noear <noear@live.cn>
2021-02-04 20:16:48 +0800

    3.2.34

--- weed3/src/main/java/org/noear/weed/DbTableQueryBase.java
+++ weed3/src/main/java/org/noear/weed/DbTableQueryBase.java
@@ -535,6 +535,10 @@ public class DbTableQueryBase<T extends DbTableQueryBase> extends WhereBase<T> i
         return select(column).getVariate().longValue(0L);
     }
 
+    public Variate selectVariate(String column) throws SQLException {
+        return select(column).getVariate();
+    }
+
     public Object selectValue(String column) throws SQLException {
         return select(column).getValue();
     }
