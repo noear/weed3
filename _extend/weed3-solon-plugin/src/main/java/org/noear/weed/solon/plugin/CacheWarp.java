ad9030472e441178505cf0359397e08752551e90
noear <noear@live.cn>
2021-05-17 17:37:19 +0800

    3.3.9

---
+++ _extend/weed3-solon-plugin/src/main/java/org/noear/weed/solon/plugin/CacheWarp.java
@@ -0,0 +1,45 @@
+package org.noear.weed.solon.plugin;
+
+import org.noear.solon.core.cache.CacheService;
+import org.noear.weed.cache.ICacheServiceEx;
+
+/**
+ * @author noear 2021/5/17 created
+ */
+public class CacheWarp implements ICacheServiceEx, CacheService {
+    public static CacheWarp warp(ICacheServiceEx cache){
+        return new CacheWarp(cache);
+    }
+
+
+    ICacheServiceEx real;
+
+    public CacheWarp(ICacheServiceEx real) {
+        this.real = real;
+    }
+
+    @Override
+    public void store(String key, Object obj, int seconds) {
+        real.store(key, obj, seconds);
+    }
+
+    @Override
+    public Object get(String key) {
+        return real.get(key);
+    }
+
+    @Override
+    public void remove(String key) {
+        real.remove(key);
+    }
+
+    @Override
+    public int getDefalutSeconds() {
+        return real.getDefalutSeconds();
+    }
+
+    @Override
+    public String getCacheKeyHead() {
+        return real.getCacheKeyHead();
+    }
+}
