e048391eef058b927bd0819ef003544a89b7139f
noear <noear@live.cn>
2021-08-06 11:33:52 +0800

    3.3.18

--- _test/src/test/java/weed3rdb/features/TableTest.java
+++ _test/src/test/java/weed3rdb/features/TableTest.java
@@ -2,6 +2,7 @@ package weed3rdb.features;
 
 import org.junit.Test;
 import org.noear.weed.DbContext;
+import org.noear.weed.DbTableQuery;
 import weed3rdb.DbUtil;
 import webapp.model.AppxModel;
 
@@ -10,11 +11,29 @@ import java.util.Map;
 public class TableTest {
     DbContext db = DbUtil.db;
 
+    public void demo() throws Exception {
+        Map<String, Object> map = db.table("appx").whereEq("app_id", 1).selectMap("*");
+
+        map.remove("app_id");
+
+        DbTableQuery tq = db.table("appx_copy")
+                .setMap(map);
+
+        if (tq.whereEq("app_id", 11).selectExists()) {
+            //在同一个 tq 里 where 会被 update 复用
+            tq.update();
+        } else {
+            tq.insert();
+        }
+    }
+
     @Test
     public void test0() throws Exception {
         Map<String, Object> map = db.table("appx").whereEq("app_id", 1).select("*").getMap();
 
         map.remove("app_id");
+
+
 
         assert db.table("appx_copy")
                 .setMap(map)
